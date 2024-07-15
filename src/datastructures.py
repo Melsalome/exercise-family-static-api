a8918a77964e422088a157cb65b3e78897ee58b4
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-07-15 23:17:21 +0200

    commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -20,17 +20,33 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        if 'id' not in member:
+            member['id'] = self._generateId()
+        if 'last_name' not in member :
+            member['last_name'] = self.last_name
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                self._members.remove(member)
+                
+                return None
+        
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
-
-    # this method is done, it returns a list with all the family members
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        return None
+            
+    def update_member(self, id, updated_member):
+        for index, member in enumerate(self._members):
+            if member['id'] == id:
+                self._members[index].update(updated_member)
+                return self._members[index]
+ 
+ 
     def get_all_members(self):
         return self._members
